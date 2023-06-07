d81b842c1b0d9d8dc5cfc169847436dd263cc6c0
Nayan Rathod <nayan.m.rathod@hotmail.com>
2023-06-07 21:16:22 +0530

    add new file for linked list with improved stretagy

---
+++ linked-list-new.py
@@ -0,0 +1,22 @@
+class Node:
+    
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+        
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+        
+    def print(self):
+        if self.head is None:
+            print('Linked list is empty')
+            return
+        
+        itr = self.head
+        
