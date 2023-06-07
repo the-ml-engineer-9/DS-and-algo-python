92656aeec49c706b94825339e52797ac9bcaa576
Nayan Rathod <nayan.m.rathod@hotmail.com>
2023-06-07 21:39:03 +0530

    linked list code complete

--- linked-list-new.py
+++ linked-list-new.py
@@ -19,4 +19,78 @@ class LinkedList:
             return
         
         itr = self.head
+        llstr = ''
+        while itr:
+            suffix = ''
+            if itr.next:
+                suffix = '-->'
+            llstr += str(itr.data) + suffix
+            itr = itr.next
+        print(llstr)
+        
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+    
+    def insert_at_end(self, data):
+        
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+            
+        itr.next = Node(data)
+        
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid index")
+        
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+            
+    def remove_at(self, index):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid index")
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1                
+        
+if __name__ == '__main__':
+    root = LinkedList()
+    root.insert_at_begining(5)
+    root.insert_at_begining(10)
+    root.insert_at_begining(15)
+    root.insert_at_end(25)
+    root.insert_at(2, 345)
+    root.remove_at(4)
+    root.print()
+    
         
